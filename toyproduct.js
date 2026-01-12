dade4755870a79263f11be7b3a487f87fcbaf2b0
Shaikh Salik <salikshaikh278@gmail.com>
2026-01-12 17:19:17 +0530

    Update toyproduct.js

--- toyproduct.js
+++ toyproduct.js
@@ -275,7 +275,7 @@ const userDataManager = new UserDataManager();
 // =================== ORDER SYSTEM (FIXED) ===================
 class OrderManager {
     constructor() {
-        this.APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxLiCqfzKH-CvCQORqGtEHvgW42LDY_j0cyE_ybk2w1PnQveELrlqm9PhrSKOcM4-by8Q/exec";
+        this.APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyTx7lwvE5LL-hgolE7OgZ-fXJi2m6zsgcZgws8M-h-5gx3iRdttpDTQ7eh31DV2OjJdw/exec";
         this.orders = JSON.parse(localStorage.getItem("abutoys_orders") || "[]");
 
         // ✅ PAGE LOAD PE BADGE UPDATE KARO
